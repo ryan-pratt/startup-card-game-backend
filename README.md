4bf3e1205190a9b9051c22d7cf59a8420e4de52e
ryan-pratt <rpratt.dev@gmail.com>
2020-07-18 22:42:42 -0600

    enabled cors

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Python (3.8) modules:
 * flask (1.1.2)
+* flask-cors (3.0.8)
 * flask-restful (0.3.8)
+* Flask-SocketIO (4.3.1)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from resources import test
 
 app = Flask(__name__)
+app.secret_key = "hewwo" # TODO
+
+# SESSION_COOKIE_DOMAIN = "http://localhost:3000"
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:3000"}})
 api = Api(app)
 
 api.add_resource(test.HelloWorld, '/test')
